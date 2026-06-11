8f5aab8a069960290710b61cd8c5741fb27234f6
cboud <cboud@example.com>
2026-06-11 15:07:51 -0700

    Create Investor handling

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Augment
 Small app as an exercise for Augment
+
+## Requirements
+1. Create a New Augment Fund
+ - Each Augment Fund must have at least:
+    - A name (e.g., “Augment Fund II”).
+    - A total number of units (e.g., 1,000).
+ - Think about how you’ll store and identify each Augment Fund (e.g., with an
+integer ID, a UUID, etc.).
+2. Retrieve an Augment Fund’s Current Cap Table
+ - The cap table shows who owns how many units, and when they acquired those
+units.
+ - Each line in the cap table must include at least:
+    - The owner’s name.
+    - The number of units they currently own.
+    - The date they acquired (or last updated) that ownership.
+ - You can decide how to structure and return this information (e.g., an array of
+objects, a JSON response, a CLI printout, etc.).
+3. Create a New Transfer
+ - A transfer is an exchange of ownership between two people for a specified
+number of units within an Augment Fund.
+ - You must update the Augment Fund’s cap table accordingly (the “from” person
+loses some units, the “to” person gains those units).
+ - You should handle relevant validations and constraints (e.g., a person can’t
+transfer more units than they own).
+4. Show the History of All Transfers for an Augment Fund
+ - Provide a way to list all past transfers that happened in a given Augment Fund,
+in some sensible order (chronological or reverse-chronological).
+ - Each transfer record should, at minimum, identify which Augment Fund it pertains
+to, who transferred what to whom, and when it occurred.
+
+
+## How To Use This Application
+
+### Return Format
+
+## Improvements I Would Make For A Production Version
+- Log Levels
+- Robust validations (eg email)
+- Email the intended recipient to invite them to Augment when someone tries to transfer shares to an Investor that does not exist.
+   - Temp hold on shares for X time until the person creates account or the go back to the original owner?
+   - Not sure on regulation for what's possible here
+   - Probably need to confirm more info from the seller
+- Need cost info on transfers
+   - Pricing/most recent cost of transaction on the fund?
---
+++ investor/investor.go
@@ -0,0 +1,83 @@
+package investor
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"fmt"
+
+	errors "augment/errors"
+	db "augment/database"
+	investorModel "augment/models"
+)
+
+func CreateInvestor(w http.ResponseWriter, r *http.Request) {
+	// Decode the request body directly into the struct
+	investor := &investorModel.Investor{}
+
+	err := json.NewDecoder(r.Body).Decode(investor)
+	if err != nil {
+		errors.WriteJSONError(w, http.StatusBadRequest, "Invalid JSON payload")
+		return
+	}
+	defer r.Body.Close()
+
+	err = investor.Validate()
+	if err != nil {
+		errors.WriteJSONError(w, http.StatusBadRequest, fmt.Sprintf("Validation error: %s", err.Error()))
+		return
+	}
+
+	// Insert the new investor into the database
+	id, err := db.InsertResource(investor)
+	// Check for duplicate email or username errors and return appropriate status and message for each, otherwise return generic internal server error for any other DB error
+	// Note that we check for both email and username duplicates in the DB function, so we can return specific errors for each case here
+	if err == db.ErrDuplicateEmail {
+		errors.WriteJSONError(w, http.StatusConflict,  fmt.Sprintf("Investor with email %s already exists", investor.Email))
+		return
+	} else if err == db.ErrDuplicateUsername {
+		errors.WriteJSONError(w, http.StatusConflict, fmt.Sprintf("Investor with username %s already exists", investor.Username))
+		return
+	} else if err == db.ErrDuplicate {
+		// Fallthrough case that should not be hit, but exists just in case to catch any duplicate case that is not specifically an email or username duplicate - return generic message since we don't know which field is duplicated
+		errors.WriteJSONError(w, http.StatusConflict, fmt.Sprintf("Investor with email %s or username %s already exists", investor.Email, investor.Username))
+		return
+	} else if err != nil && err != db.ErrDuplicate {
+		errors.WriteJSONError(w, http.StatusInternalServerError, "Failed to create investor")
+		return
+	}
+
+	log.Printf("New investor created with ID: %d", id)
+
+	// Get new investor object to return in response - returning just ID will not show accurate timestamps
+	// for the created investor
+	newInvestor, err := GetInvestorByID(id)
+	if err != nil {
+		errors.WriteJSONError(w, http.StatusInternalServerError, "Failed to retrieve created investor")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newInvestor)
+}
+
+// TODO in real-world app: handler for API call to get investor by ID and validate
+// Then call the below function to retrieve the investor and return in response
+
+func GetInvestorByID(id int64) (investor *investorModel.Investor, err error) {
+	log.Printf("Retrieving investor with ID: %d", id)
+
+	// Get the investor from the database using the generic GetResourceByID function
+	// Returns any type for use with multiple tables, so we need to cast it to an Investor below
+	res, err := db.GetResourceByID("investors", id)
+	if err != nil {
+		return nil, err
+	}
+
+	investor, ok := res.(*investorModel.Investor)
+	if !ok {
+		return nil, fmt.Errorf("Failed to cast resource to Investor model for ID %d", id)
+	}
+
+	return investor, nil
+}
